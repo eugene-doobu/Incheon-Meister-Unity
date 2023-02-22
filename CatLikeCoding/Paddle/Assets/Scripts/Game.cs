d8e16e480e38056d4536e419a2a8b4130b93aba2
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-02-22 23:38:14 +0900

    1-game scene

---
+++ CatLikeCoding/Paddle/Assets/Scripts/Game.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Ball   ball;
+    [SerializeField] private Paddle bottomPaddle, topPaddle;
+}
