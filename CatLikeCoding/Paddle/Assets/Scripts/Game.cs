bc3c74d1d0ea2aa5785194ffe546c6799f908217
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-04-23 23:05:27 +0900

    Controlling the Ball

--- CatLikeCoding/Paddle/Assets/Scripts/Game.cs
+++ CatLikeCoding/Paddle/Assets/Scripts/Game.cs
@@ -1,9 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    [SerializeField] private Ball   ball;
-    [SerializeField] private Paddle bottomPaddle, topPaddle;
+    [SerializeField]          private Ball    ball;
+    [SerializeField]          private Paddle  bottomPaddle, topPaddle;
+    [SerializeField, Min(0f)] private Vector2 arenaExtents = new Vector2(10f, 10f);
+
+    private void Awake()
+    {
+        ball.StartNewGame();
+    }
+
+    private void Update()
+    {
+        bottomPaddle.Move(ball.Position.x, arenaExtents.x);
+        topPaddle.Move(ball.Position.x, arenaExtents.x);
+        ball.Move();
+        BounceYIfNeeded();
+        BounceXIfNeeded();
+        ball.UpdateVisualization();
+    }
+
+    void BounceYIfNeeded()
+    {
+        float yExtents = arenaExtents.y - ball.Extents;
+        if (ball.Position.y < -yExtents)
+        {
+            ball.BounceY(-yExtents);
+        }
+        else if (ball.Position.y > yExtents)
+        {
+            ball.BounceY(yExtents);
+        }
+    }
+    
+    void BounceXIfNeeded()
+    {
+        float xExtents = arenaExtents.x - ball.Extents;
+        if (ball.Position.x < -xExtents)
+        {
+            ball.BounceX(-xExtents);
+        }
+        else if (ball.Position.x > xExtents)
+        {
+            ball.BounceX(xExtents);
+        }
+    }
 }
--- CatLikeCoding/Paddle/Assets/Scripts/Paddle.cs
+++ CatLikeCoding/Paddle/Assets/Scripts/Paddle.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    void Start()
+    [SerializeField, Min(0f)] private float
+        extents = 4f,
+        speed   = 10f;
+
+    [SerializeField] private bool isAi;
+
+    public void Move(float target, float arenaExtents)
     {
-        
+        var p = transform.localPosition;
+        p.x = isAi ? AdjustByAI(p.x, target) : AdjustByPlayer(p.x);
+        var limit = arenaExtents - extents;
+        p.x = Mathf.Clamp(p.x, -limit, limit);
+        transform.localPosition = p;
     }
 
-    void Update()
+    float AdjustByAI(float x, float target)
     {
-        
+        if (x < target)
+        {
+            return Mathf.Min(x + speed * Time.deltaTime, target);
+        }
+        return Mathf.Min(x - speed * Time.deltaTime, target);
+    }
+    
+    float AdjustByPlayer(float x)
+    {
+        bool goRight = Input.GetKey(KeyCode.RightArrow);
+        bool goLeft  = Input.GetKey(KeyCode.LeftArrow);
+        if (goRight && !goLeft)
+        {
+            return x + speed * Time.deltaTime;
+        }
+        if (goLeft && !goRight)
+        {
+            return x - speed * Time.deltaTime;
+        }
+        return x;
     }
 }
